787a09fb02d4779ea557563930441c589fd07561
SergiyStasyuk <sta_s@ukr.net>
2018-09-16 10:18:06 +0300

    Add parameter validation

--- app.rb
+++ app.rb
@@ -9,12 +9,21 @@ def init_db
 	@db.results_as_hash = true
 end
 
+# before вызывается каждый раз при перезагрузке
+# любой страницы
+
 before do
+	# инициализация БД
 	init_db
 end
 
+# configure вызывается каждый раз при конфигурации приложения:
+# когда изменился код программы и перезагрузилась страница
+
 configure do
+	# инициализация БД
 	init_db
+	# создает таблицу если таблица не существует
 	@db.execute 'create table if not exists Posts
 	(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,12 +36,24 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!</a>"			
 end
 
+#обработчик get-запроса /new
+# (браузер получает страницу с сервера)
+
 get '/new' do
   erb :new
 end
 
+#обработчик post-запроса /new
+# (браузер отправляет данные на сервер)
+
 post '/new' do
+  #получаем переменную из пост запроса
   content = params[:content]
+
+	if content.length <= 0
+		@error = 'Type post text'
+		return erb :new
+	end
 
   erb "Tou typed #{content}"
 end
